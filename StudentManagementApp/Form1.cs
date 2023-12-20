5e8988060eb19586fd2fc4a49771d729a8350513
Cengizhan Şahin <cengizhan7sahin@gmail.com>
2023-12-20 19:47:49 +0300

    Add project files.

---
+++ StudentManagementApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementApp
+{
+    public partial class Form1 : Form
+    {
+        public static string connectionString = "Server=DESKTOP-S5SNUUO;Database=DB_STUDENT;Trusted_Connection=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void RefreshData()
+        {
+            string query = "SELECT * FROM Students";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+        }
+    }
+}
